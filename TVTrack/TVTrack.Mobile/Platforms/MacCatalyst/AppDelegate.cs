c47aa8c52c5eb7b5311de6cb0ece273480129d48
Jaroslav Hort <hortjaroslav0@gmail.com>
2023-04-08 14:47:20 +0200

    chore: remove iOS/Mac and Tizen platforms we are not targeting

--- TVTrack/TVTrack.Mobile/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace TVTrack.Mobile;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
