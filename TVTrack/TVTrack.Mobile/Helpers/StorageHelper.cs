3eae645abe280437c138658e3746e9318940bd3e
hojkas <ivka.strnadova@gmail.com>
2023-04-19 22:28:17 +0200

    feat: storage helper with temporary testing username

---
+++ TVTrack/TVTrack.Mobile/Helpers/StorageHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVTrack.Mobile.Helpers;
+public static class StorageHelper
+{
+    public async static Task StoreUsername(string username)
+    {
+        await SecureStorage.Default.SetAsync("username", username);
+    }
+
+    public async static Task<string> GetUsername()
+    {
+        return "test";
+        // return await SecureStorage.Default.GetAsync("username");
+    }
+}
